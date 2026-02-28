419cc3bff7b2bbec3fe03ae6ba57489d7299d8d8
ARNAB GHOSH <you@example.com>
2026-02-28 19:49:49 +0530

    Add initial files

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'dotenv/config'
+import cors from 'cors';
+
+
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('API is running....')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log('server is running on port '+ PORT); 
+})
+
+export default app;
+
+
+
